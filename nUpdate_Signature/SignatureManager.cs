e973586609de3b7c36265bf16ac147a91600c810
Trade <smile.nicki@yahoo.de>
2014-01-03 22:16:41 +0100

    Create SignatureManager.cs

---
+++ nUpdate_Signature/SignatureManager.cs
@@ -0,0 +1,88 @@
+/**
+ *  Klasse zum Signieren von Daten mithilfe des RSA-Algorithmus
+ *  
+ *  Author: Tim Schiewe (timmi31061)
+ *  Lizenz: GPL v3
+ *  Erstellt: 09. Dezember 2013
+ *  Modifiziert:  Trade - 15.12.13 - Design des Konstruktors verbessert
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+
+namespace nUpdate.SignatureManager
+{
+    public class RsaSignature
+    {
+        /// <summary>
+        /// Die Standardschlüsselgröße in Bits
+        /// </summary>
+        public const int DefaultKeySize = 4096; 
+
+        private RSACryptoServiceProvider rsa;
+
+        /// <summary>
+        /// Gibt den öffentlichen Schlüssel zurück.
+        /// </summary>
+        public string PublicKey
+        {
+            get
+            {
+                return this.rsa.ToXmlString(false); // Öffentlichen Schlüssel exportieren.
+            }
+        }
+
+        /// <summary>
+        /// Gibt den privaten Schlüssel zurück.
+        /// </summary>
+        public string PrivateKey
+        {
+            get
+            {
+                return this.rsa.ToXmlString(true);  // Privaten und öffentlichen Schlüssel exportieren.
+            }
+        }
+
+        /// <summary>
+        /// Erstellt eine neue RsaSignature-Instanz.
+        /// </summary>
+        /// <param name="rsaKey">Der zu verwendene RSA-Schlüssel. Wenn keiner angegeben wurde, wird ein neuer erstellt.</param>
+        public RsaSignature(string rsaKey)
+        {
+                this.rsa = new RSACryptoServiceProvider();  // Schlüssel wurde angegeben...
+                this.rsa.FromXmlString(rsaKey);             // ...also wird dieser importiert.
+            
+        }
+
+        public RsaSignature()
+        {
+            this.rsa = new RSACryptoServiceProvider(DefaultKeySize);    // Kein Schlüssel angegeben, einen neuen mit der Standardschlüsselgröße erstellen.
+            this.rsa.ToXmlString(true);                                 // Ein Dummy, um den Schlüssel zu erstellen.
+        }
+
+        /// <summary>
+        /// Berechnet die Signatur für die angegebenen Daten.
+        /// </summary>
+        /// <param name="data">Die Daten, für die die Signatur berechnet werden soll.</param>
+        /// <returns>Die berechnete Signatur.</returns>
+        public byte[] SignData(byte[] data)
+        {
+            return this.rsa.SignData(data, typeof(SHA512));  // Berechnet die Signatur und gibt sie zurück.
+        }
+
+        /// <summary>
+        /// Überprüft die angegebene Signatur für die angegebenen Daten.
+        /// </summary>
+        /// <param name="data">Die Daten, für die die Signatur überprüft werden soll.</param>
+        /// <param name="signature">Die Signatur, die überprüft werden soll.</param>
+        /// <returns>True, wenn die Signatur gültig ist, andernfalls false.</returns>
+        public bool VerifyData(byte[] data, byte[] signature)
+        {
+            return this.rsa.VerifyData(data, typeof(SHA512), signature); // Überprüft, ob die berechnete Signatur für diese Daten gültig ist.
+        }
+    }
+}
