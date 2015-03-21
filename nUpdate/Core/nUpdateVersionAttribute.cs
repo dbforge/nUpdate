ce0e78f8fb76000962f9c7f3d06a8920d70cfd4a
Jöran Malek <joeran3@gmail.com>
2015-03-21 08:07:37 +0100

    nUpdateVersionAttribute. Not tested, cause of missing libraries.

---
+++ nUpdate/Core/nUpdateVersionAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace nUpdate.Core
+{
+	[AttributeUsage(AttributeTargets.Assembly)]
+	public sealed class nUpdateVersionAttribute : Attribute
+	{
+		private readonly string nUpdateVersionString;
+
+		public string VersionString
+		{
+			get { return nUpdateVersionString; }
+		}
+
+		public nUpdateVersionAttribute(string nUpdateVersionString)
+		{
+			this.nUpdateVersionString = nUpdateVersionString;
+		}
+	}
+}
