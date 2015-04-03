e5d4e3e0259cc535427c90547c308c72571080ad
ProgTrade <admin@trade-programming.de>
2015-04-03 18:36:25 +0200

    Adjusted installer to changes and added arguments

---
+++ nUpdate.UpdateInstaller/InstallerServiceProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using nUpdate.UpdateInstaller;
+using nUpdate.UpdateInstaller.Client.GuiInterface;
+
+[assembly: ServiceProvider(typeof(InstallerServiceProvider))]
+namespace nUpdate.UpdateInstaller
+{
+    public class InstallerServiceProvider : IServiceProvider
+    {
+        private readonly Dictionary<Type, object> _services;
+
+        public InstallerServiceProvider()
+        {
+            _services = new Dictionary<Type, object>();
+            InitializeServices();
+        }
+
+        private void InitializeServices()
+        {
+            _services.Add(typeof(IProgressReporter), new ProgressReporterService());
+        }
+
+        public object GetService(Type serviceType)
+        {
+            if (serviceType == null)
+                throw new ArgumentNullException("serviceType");
+            object service;
+            return !_services.TryGetValue(serviceType, out service) ? null : service;
+        }
+    }
+}
---
+++ nUpdate.UpdateInstaller/ServiceProviderHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using nUpdate.UpdateInstaller.Client.GuiInterface;
+
+namespace nUpdate.UpdateInstaller
+{
+    internal class ServiceProviderHelper
+    {
+        public static IServiceProvider CreateServiceProvider(System.Reflection.Assembly assembly)
+        {
+            if (assembly == null)
+                throw new ArgumentNullException("assembly");
+
+            var attribute = assembly.GetCustomAttributes(typeof(ServiceProviderAttribute), false).Cast<ServiceProviderAttribute>().SingleOrDefault();
+
+            if (attribute == null)
+                return null;
+
+            return (IServiceProvider)Activator.CreateInstance(attribute.ServiceType);
+        }
+    }
+}
