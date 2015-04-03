2c00c0fbc0eae08c3068f198be2a28538ac0d999
ProgTrade <admin@trade-programming.de>
2015-04-03 18:22:50 +0200

    Adjusted updater in administration

---
+++ nUpdate/Updating/UpdateArgument.cs
@@ -0,0 +1,29 @@
+﻿namespace nUpdate.Updating
+{
+    /// <summary>
+    ///     Represents an argument that is handled over to the application after the installation of an update.
+    /// </summary>
+    public class UpdateArgument
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="UpdateArgument"/> class.
+        /// </summary>
+        /// <param name="argument">The argument.</param>
+        /// <param name="executionOptions">The execution options that should be used for the <see cref="UpdateArgument"/>.</param>
+        public UpdateArgument(string argument, UpdateArgumentExecutionOptions executionOptions)
+        {
+            Argument = argument;
+            ExecutionOptions = executionOptions;
+        }
+
+        /// <summary>
+        ///     Gets or sets the execution options that should be used for the current <see cref="UpdateArgument"/>.
+        /// </summary>
+        public UpdateArgumentExecutionOptions ExecutionOptions { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the argument of the current <see cref="UpdateArgument"/>.
+        /// </summary>
+        public string Argument { get; set; }
+    }
+}
