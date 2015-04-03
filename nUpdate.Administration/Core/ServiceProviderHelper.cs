57cf78b132bbd597bfb868119fedc96e772d8d6c
ProgTrade <admin@trade-programming.de>
2015-04-03 18:17:45 +0200

    Implemented service providers

---
+++ nUpdate.Administration/Core/ServiceProviderHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using nUpdate.Administration.TransferInterface;
+
+namespace nUpdate.Administration.Core
+{
+    public class ServiceProviderHelper
+    {
+        public static IServiceProvider CreateServiceProvider(System.Reflection.Assembly assembly)
+        {
+            if (assembly == null)
+                throw new ArgumentNullException("assembly");
+
+            var attribute =
+                assembly.GetCustomAttributes(typeof (ServiceProviderAttribute), false)
+                    .Cast<ServiceProviderAttribute>()
+                    .SingleOrDefault();
+
+            if (attribute == null)
+                return null;
+
+            return (IServiceProvider) Activator.CreateInstance(attribute.ServiceType);
+        }
+    }
+}
