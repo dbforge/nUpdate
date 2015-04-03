c83a76f2f724813f6e3ebd5cbb6e030648634384
ProgTrade <admin@trade-programming.de>
2015-04-03 18:30:01 +0200

    Changed methods to properties in SystemInformation

--- nUpdate/Core/SystemInformation.cs
+++ nUpdate/Core/SystemInformation.cs
@@ -9,51 +9,55 @@ namespace nUpdate.Core
     internal class SystemInformation
     {
         /// <summary>
-        ///     Gets the Mac-Address of the current computer for the statistics.
+        ///     Gets the MAC-Address of the current computer for the statistics.
         /// </summary>
-        /// <returns>Returns the found mac address.</returns>
-        public static string GetMacAddress()
+        public static string MacAddress
         {
-            return
-                NetworkInterface.GetAllNetworkInterfaces()
-                    .Where(nic => nic.OperationalStatus == OperationalStatus.Up)
-                    .Select(nic => nic.GetPhysicalAddress().ToString())
-                    .FirstOrDefault();
+            get
+            {
+                return
+                    NetworkInterface.GetAllNetworkInterfaces()
+                        .Where(nic => nic.OperationalStatus == OperationalStatus.Up)
+                        .Select(nic => nic.GetPhysicalAddress().ToString())
+                        .FirstOrDefault();
+            }
         }
 
         /// <summary>
         ///     Gets the name of the current computer's operating system.
         /// </summary>
-        /// <returns>The found operating system name.</returns>
-        public static string GetOperatingSystemName()
+        public static string OperatingSystemName
         {
-            var osVersion = Environment.OSVersion.Version;
-            switch (osVersion.Major)
+            get
             {
-                case 6:
-                    switch (osVersion.Minor)
-                    {
-                        case 0:
-                            return "Windows Vista";
-                        case 1:
-                            return "Windows 7";
-                        case 2:
-                            return "Windows 8";
-                        case 3:
-                            return "Windows 8.1";
-                    }
-                    break;
+                var osVersion = Environment.OSVersion.Version;
+                switch (osVersion.Major)
+                {
+                    case 6:
+                        switch (osVersion.Minor)
+                        {
+                            case 0:
+                                return "Windows Vista";
+                            case 1:
+                                return "Windows 7";
+                            case 2:
+                                return "Windows 8";
+                            case 3:
+                                return "Windows 8.1";
+                        }
+                        break;
 
-                case 10:
-                    switch (osVersion.Minor)
-                    {
-                        case 0:
-                            return "Windows 10";
-                    }
-                    break;
+                    case 10:
+                        switch (osVersion.Minor)
+                        {
+                            case 0:
+                                return "Windows 10";
+                        }
+                        break;
+                }
+
+                return "Unknown";
             }
-
-            return "Unknown";
         }
     }
 }
