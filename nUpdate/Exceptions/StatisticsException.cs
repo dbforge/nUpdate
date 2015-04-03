c0a0ec614b94fa919cff764eff882ccda943aea9
Dominic Beger <info@dominic-beger.de>
2015-04-03 18:22:50 +0200

    Adjusted updater in administration

---
+++ nUpdate/Exceptions/StatisticsException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace nUpdate.Exceptions
+{
+    /// <summary>
+    ///     The exception that is thrown if the statistics entry fails.
+    /// </summary>
+    [Serializable]
+    public class StatisticsException : Exception
+    {
+        public StatisticsException(string message)
+            : base(message)
+        {
+        }
+    }
+}
