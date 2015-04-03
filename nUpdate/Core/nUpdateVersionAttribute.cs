eaa32372cb884ac0df41713f9dbc418802060885
Dominic Beger <info@dominic-beger.de>
2015-04-03 18:19:57 +0200

    Added nUpdateVersionAttribute
    This attribute lets the user provide the version to use and is the
    interface between host application and the code parts where the version
    is necessary.

--- nUpdate/Core/nUpdateVersionAttribute.cs
+++ nUpdate/Core/nUpdateVersionAttribute.cs
@@ -2,19 +2,31 @@
 
 namespace nUpdate.Core
 {
-	[AttributeUsage(AttributeTargets.Assembly)]
-	public sealed class nUpdateVersionAttribute : Attribute
-	{
-		private readonly string nUpdateVersionString;
+    /// <summary>
+    ///     Represents an attribute that will be used to determine the current version in the constructor of the <see cref="UpdateManager"/> class.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly)]
+    // ReSharper disable once InconsistentNaming
+    public sealed class nUpdateVersionAttribute : Attribute
+    {
+        private readonly string _nUpdateVersionString;
 
-		public string VersionString
-		{
-			get { return nUpdateVersionString; }
-		}
+        /// <summary>
+        ///     Gets the version string.
+        /// </summary>
+        public string VersionString
+        {
+            get { return _nUpdateVersionString; }
+        }
 
-		public nUpdateVersionAttribute(string nUpdateVersionString)
-		{
-			this.nUpdateVersionString = nUpdateVersionString;
-		}
-	}
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="nUpdateVersionAttribute"/> class.
+        /// </summary>
+        /// <param name="nUpdateVersionString">The version string to use.</param>
+        /// <remarks>The version string will be represented in an instance of the <see cref="UpdateVersion"/> class.</remarks>
+        public nUpdateVersionAttribute(string nUpdateVersionString)
+        {
+            _nUpdateVersionString = nUpdateVersionString;
+        }
+    }
 }
