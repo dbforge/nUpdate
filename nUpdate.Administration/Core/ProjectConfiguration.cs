79d193ee5e02c5f9029667f78a41c32a6130b5c3
Dominic Beger <info@dominic-beger.de>
2015-04-03 18:18:48 +0200

    Simplified project configuration management

---
+++ nUpdate.Administration/Core/ProjectConfiguration.cs
@@ -0,0 +1,40 @@
+﻿// Author: Dominic Beger (Trade/ProgTrade)
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using nUpdate.Core;
+
+namespace nUpdate.Administration.Core
+{
+    [Serializable]
+    public class ProjectConfiguration
+    {
+        public ProjectConfiguration(string name, string path)
+        {
+            Name = name;
+            Path = path;
+        }
+
+        /// <summary>
+        ///     The name of the project.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        ///     The path o´f the project-file.
+        /// </summary>
+        public string Path { get; set; }
+
+        /// <summary>
+        ///     Loads the project configuration from the file in the application's main folder.
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<ProjectConfiguration> Load()
+        {
+            return
+                Serializer.Deserialize<IEnumerable<ProjectConfiguration>>(
+                    File.ReadAllText(Program.ProjectsConfigFilePath));
+        }
+    }
+}
