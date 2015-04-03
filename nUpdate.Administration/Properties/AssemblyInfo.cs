c3a5c4f889c51174bf80af715356c76dc8eb30d8
ProgTrade <admin@trade-programming.de>
2015-04-03 18:19:57 +0200

    Added nUpdateVersionAttribute
    This attribute lets the user provide the version to use and is the
    interface between host application and the code parts where the version
    is necessary.

---
+++ nUpdate.Administration/Properties/AssemblyInfo.cs
@@ -0,0 +1,47 @@
+﻿// Author: Dominic Beger (Trade/ProgTrade)
+
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+using nUpdate.Core;
+
+[assembly: AssemblyTitle("nUpdate Administration")]
+[assembly: AssemblyDescription("The administration of the nUpdate library.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("nUpdate Administration")]
+[assembly: AssemblyCopyright("Copyright © Dominic Beger (Trade/ProgTrade) 2014-2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+
+[assembly: ComVisible(true)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+
+[assembly: Guid("8935ccec-fd71-4b00-9c81-59dec07f533c")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("v1.0.0.0 Beta 1")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("")]
+[assembly: nUpdateVersion("1.0b1")]
+[assembly: Exceptionless.Configuration.Exceptionless("ZmNZSHjnJ6DrGPoWHxAgxlP491Plche8G7gkk87p")]
