f52aea59f18f48b68af043c5981e9f81e30e5483
Trade-Programming.de <admin@trade-programming.de>
2015-08-25 18:14:36 +0200

    Renamed ConnectionChecker to WebConnection

---
+++ nUpdate/WebConnection.cs
@@ -0,0 +1,19 @@
+﻿// Author: Dominic Beger (Trade/ProgTrade)
+
+using nUpdate.Win32;
+
+namespace nUpdate
+{
+    public class WebConnection
+    {
+        /// <summary>
+        ///     Checks whether an internet connection is available, or not.
+        /// </summary>
+        /// <returns>Returns <c>true</c> if a network connection is available, otherwise <c>false</c>.</returns>
+        public static bool IsAvailable()
+        {
+            int desc;
+            return NativeMethods.InternetGetConnectedState(out desc, 0);
+        }
+    }
+}
