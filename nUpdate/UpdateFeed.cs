0fc7677be1a50693739ade1c0cd0eaa0679d9df5
Dominic Beger <admin@trade-programming.de>
2016-07-30 21:40:34 +0200

    Preparements for changes in the versioning system

---
+++ nUpdate/UpdateFeed.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace nUpdate
+{
+    [Serializable]
+    public class UpdateFeed
+    {
+        public string Name { get; set; }
+
+        public Uri Feed { get; set; }
+
+        public Version LatestVersion { get; set; }
+    }
+}
