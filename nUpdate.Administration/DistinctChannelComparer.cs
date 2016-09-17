def12876eca012c9d9e8a41ba398367d15dbd13b
Dominic Beger <admin@trade-programming.de>
2016-09-18 01:46:50 +0200

    Add DistinctChannelComparer

---
+++ nUpdate.Administration/DistinctChannelComparer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace nUpdate.Administration
+{
+    internal class DistinctChannelComparer : IEqualityComparer<UpdatePackage>
+    {
+        public bool Equals(UpdatePackage left, UpdatePackage right)
+        {
+            return left.ChannelName == right.ChannelName;
+        }
+
+        public int GetHashCode(UpdatePackage obj)
+        {
+            return obj.ChannelName.GetHashCode();
+        }
+    }
+}
