effeef08d35fb1422191d560edcd21f3f4bca6dd
Dominic Beger <info@dominic-beger.de>
2017-03-11 19:56:07 +0100

    Implement host application options

---
+++ nUpdate/HostApplicationOptions.cs
@@ -0,0 +1,24 @@
+﻿namespace nUpdate
+{
+    /// <summary>
+    ///     Represents different behaviours of the host application when the update installation is started.
+    /// </summary>
+    public enum HostApplicationOptions
+    {
+        /// <summary>
+        ///     Leaves the host application open and does not restart it after the update installation.
+        /// </summary>
+        /// <remarks>This option is only recommended, if no program files are changed.</remarks>
+        LeaveOpen,
+
+        /// <summary>
+        ///     Closes the host application and does not restart it after the update installation.
+        /// </summary>
+        Close,
+
+        /// <summary>
+        ///     Closes the host application and restarts it after the update installation.
+        /// </summary>
+        CloseAndRestart
+    }
+}
