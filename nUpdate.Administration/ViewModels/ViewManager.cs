b370abf6876fba97aa23d7887bad3c6a0cb569f0
Dominic Beger <info@dominic-beger.de>
2017-07-02 14:48:19 +0200

    Rename ViewManagerBase to ViewManager

--- nUpdate.Administration/ViewModels/ViewManager.cs
+++ nUpdate.Administration/ViewModels/ViewManager.cs
@@ -9,16 +9,56 @@ namespace nUpdate.Administration.ViewModels.Base
     /// <summary>
     ///     Provides an abstract view manager class that offers basic convertion methods for finding the corresponding view for a specified view model by using an <see cref="IValueConverter"/>. 
     /// </summary>
-    public abstract class ViewManagerBase : IValueConverter
+    public abstract class ViewManager : IValueConverter
     {
-        // Here we save the types of the view models and their corresponding views. This will be set by the inheriting class.
-        private readonly Dictionary<Type, Type> _viewModelViewDictionary;
-        // Here we save the types of the view models and the instances of their views.
+        /// <summary>
+        ///     Saves the types of the view models and their corresponding views.
+        /// </summary>
+        private readonly Dictionary<Type, Type> _viewTypes = new Dictionary<Type, Type>();
+
+        /// <summary>
+        ///      Saves the types of the view models and an instance of their view.
+        /// </summary>
         private readonly Dictionary<Type, FrameworkElement> _views = new Dictionary<Type, FrameworkElement>();
 
-        protected ViewManagerBase(Dictionary<Type, Type> v)
+        protected ViewManager()
+        { }
+
+        protected ViewManager(Dictionary<Type, Type> viewTypes)
         {
-            _viewModelViewDictionary = v;
+            AddViewTypes(viewTypes);
+        }
+
+        protected ViewManager(Dictionary<Type, FrameworkElement> views)
+        {
+            AddViews(views);
+        }
+
+        protected void AddViewType(Type viewModelType, Type viewType)
+        {
+            _viewTypes.Add(viewModelType, viewType);
+        }
+
+        protected void AddViewTypes(Dictionary<Type, Type> viewTypes)
+        {
+            foreach (var entry in viewTypes)
+            {
+                AddViewType(entry.Key, entry.Value);
+            }
+        }
+
+        protected void AddView(Type viewModelType, FrameworkElement view)
+        {
+            AddViewType(viewModelType, view.GetType());
+            _views.Add(viewModelType, view);
+        }
+
+        protected void AddViews(Dictionary<Type, FrameworkElement> views)
+        {
+            foreach (var entry in views)
+            {
+                AddView(entry.Key, entry.Value);
+            }
         }
 
         /// <summary>
@@ -47,7 +87,7 @@ namespace nUpdate.Administration.ViewModels.Base
                 throw new ArgumentNullException(nameof(value));
 
             var viewModelType = value.GetType();
-            return !_viewModelViewDictionary.ContainsKey(viewModelType) ? null : GetViewInstance(viewModelType, value, _viewModelViewDictionary[viewModelType]);
+            return !_viewTypes.ContainsKey(viewModelType) ? null : GetViewInstance(viewModelType, value, _viewTypes[viewModelType]);
         }
         
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
