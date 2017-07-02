f4fb6b0671714f824e67c969c70eaba73201afab
Dominic Beger <admin@trade-programming.de>
2017-07-02 14:49:32 +0200

    Make old view models inherit from new ViewModel class

--- nUpdate.Administration/ViewModels/MainWindowViewModel.cs
+++ nUpdate.Administration/ViewModels/MainWindowViewModel.cs
@@ -4,31 +4,43 @@ using System.ComponentModel;
 using System.Windows.Data;
 using System.Windows.Media.Imaging;
 using nUpdate.Administration.Infrastructure;
+using nUpdate.Administration.Views;
 
 namespace nUpdate.Administration.ViewModels
 {
-    public class MainWindowViewModel : PropertyChangedBase
+    public class MainWindowViewModel : ViewModel
     {
         public MainWindowViewModel()
         {
             CollectionView =
                 new ListCollectionView(new List<MainMenuItemViewModel>
                 {
-                    new MainMenuItemViewModel("New project", "Creates a new project linked with your application.", MainMenuGroup.Projects, new BitmapImage(new Uri(@"/nUpdate.Administration;component/Icons/NewSolutionFolder_64x.png", UriKind.Relative)), new RelayCommand(null, null)),
+                    new MainMenuItemViewModel("New project", "Creates a new project linked with your application.", MainMenuGroup.Projects, new BitmapImage(new Uri(@"/nUpdate.Administration;component/Icons/NewSolutionFolder_64x.png", UriKind.Relative)), new RelayCommand(
+                        () =>
+                        {
+                            var newProjectWindow = new NewProjectWindow();
+                            newProjectWindow.ShowDialog();
+                        })),
 
-                    new MainMenuItemViewModel("Open project", "Opens an existing project for managing its updates.", MainMenuGroup.Projects, new BitmapImage(new Uri(@"/nUpdate.Administration;component/Icons/ProjectFolderOpen_64x.png", UriKind.Relative)), new RelayCommand(null, null)),
+                    new MainMenuItemViewModel("Open project", "Opens an existing project for managing its updates.", MainMenuGroup.Projects, new BitmapImage(new Uri(@"/nUpdate.Administration;component/Icons/ProjectFolderOpen_64x.png", UriKind.Relative)), new RelayCommand(
+                        () => { })),
 
-                    new MainMenuItemViewModel("Manage key database", "Import, export or edit your passwords or private keys.", MainMenuGroup.KeyDatabase, new BitmapImage(new Uri(@"/nUpdate.Administration;component/Icons/Key_64x.png", UriKind.Relative)), new RelayCommand(null, null)),
+                    new MainMenuItemViewModel("Manage key database", "Import, export or edit your passwords or private keys.", MainMenuGroup.KeyDatabase, new BitmapImage(new Uri(@"/nUpdate.Administration;component/Icons/Key_64x.png", UriKind.Relative)), new RelayCommand(
+                        () => { })),
 
-                    new MainMenuItemViewModel("Change master password", "Change the password used for encrypting the key database.", MainMenuGroup.KeyDatabase, new BitmapImage(new Uri(@"/nUpdate.Administration;component/Icons/Lock_64x.png", UriKind.Relative)), new RelayCommand(null, CanEditMasterPassword)),
+                    new MainMenuItemViewModel("Change master password", "Change the password used for encrypting the key database.", MainMenuGroup.KeyDatabase, new BitmapImage(new Uri(@"/nUpdate.Administration;component/Icons/Lock_64x.png", UriKind.Relative)), new RelayCommand(() => {}, CanEditMasterPassword)),
 
-                    new MainMenuItemViewModel("Manage statistics servers", "Add, edit or delete statistics servers.", MainMenuGroup.Statistics, new BitmapImage(new Uri(@"/nUpdate.Administration;component/Icons/ServerReport_64x.png", UriKind.Relative)), new RelayCommand(null, null)),
+                    new MainMenuItemViewModel("Manage statistics servers", "Add, edit or delete statistics servers.", MainMenuGroup.Statistics, new BitmapImage(new Uri(@"/nUpdate.Administration;component/Icons/ServerReport_64x.png", UriKind.Relative)), new RelayCommand(
+                        () => { })),
 
-                    new MainMenuItemViewModel("Preferences", "Customize nUpdate Administration.", MainMenuGroup.Application, new BitmapImage(new Uri(@"/nUpdate.Administration;component/Icons/Settings_64x.png", UriKind.Relative)), new RelayCommand(null, null)),
+                    new MainMenuItemViewModel("Preferences", "Customize nUpdate Administration.", MainMenuGroup.Application, new BitmapImage(new Uri(@"/nUpdate.Administration;component/Icons/Settings_64x.png", UriKind.Relative)), new RelayCommand(
+                        () => { })),
 
-                    new MainMenuItemViewModel("Information", "About nUpdate Administration.", MainMenuGroup.Application, new BitmapImage(new Uri(@"/nUpdate.Administration;component/Icons/CodeInformation_64x.png", UriKind.Relative)), new RelayCommand(null, null)),
+                    new MainMenuItemViewModel("Information", "About nUpdate Administration.", MainMenuGroup.Application, new BitmapImage(new Uri(@"/nUpdate.Administration;component/Icons/CodeInformation_64x.png", UriKind.Relative)), new RelayCommand(
+                        () => { })),
 
-                    new MainMenuItemViewModel("Feedback", "Send your feedback right here.", MainMenuGroup.Application, new BitmapImage(new Uri(@"/nUpdate.Administration;component/Icons/feedbacktoolicon_64x.png", UriKind.Relative)), new RelayCommand(null, null))
+                    new MainMenuItemViewModel("Feedback", "Send your feedback right here.", MainMenuGroup.Application, new BitmapImage(new Uri(@"/nUpdate.Administration;component/Icons/feedbacktoolicon_64x.png", UriKind.Relative)), new RelayCommand(
+                        () => { }))
                 });
             CollectionView.GroupDescriptions.Add(new PropertyGroupDescription("Category"));
         }
