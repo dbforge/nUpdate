09f7b66aa90f763cca86e2dfeaa896237f09f8fd
Dominic Beger <info@dominic-beger.de>
2017-07-02 14:45:21 +0200

    Add basic content to the NewProjectWindow

--- nUpdate.Administration/Views/NewProjectWindow.xaml.cs
+++ nUpdate.Administration/Views/NewProjectWindow.xaml.cs
@@ -1,23 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
-namespace nUpdate.Administration.Views
+﻿namespace nUpdate.Administration.Views
 {
     /// <summary>
     /// Interaction logic for NewProjectWindow.xaml
     /// </summary>
-    public partial class NewProjectWindow : Window
+    public partial class NewProjectWindow
     {
         public NewProjectWindow()
         {
