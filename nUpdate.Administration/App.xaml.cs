5b9d20d7232fdedf3f524c1a5366c999c15490bc
Dominic Beger <info@dominic-beger.de>
2017-07-22 20:59:15 +0200

    Comment the custom ApplicationStart method due to the new WindowManager

--- nUpdate.Administration/App.xaml.cs
+++ nUpdate.Administration/App.xaml.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace nUpdate.Administration
+﻿namespace nUpdate.Administration
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        /*private void ApplicationStart(object sender, StartupEventArgs e)
+        {
+            //Disable shutdown when the dialog closes
+            Current.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+            WindowManager.Show(new MainWindow());
+        }*/
     }
 }
