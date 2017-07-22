455fb41925e4f5893bd09dd6d160448b3eece574
Dominic Beger <info@dominic-beger.de>
2017-07-22 20:51:09 +0200

    Add further options for the pages inside of the wizard

--- nUpdate.Administration/ViewModels/PageViewModel.cs
+++ nUpdate.Administration/ViewModels/PageViewModel.cs
@@ -1,22 +1,39 @@
-﻿namespace nUpdate.Administration.ViewModels
+﻿// Author: Dominic Beger (Trade/ProgTrade) 2017
+
+namespace nUpdate.Administration.ViewModels
 {
     /// <summary>
     ///     Represents a view model for a wizard page.
     /// </summary>
     public abstract class PageViewModel : ViewModel
     {
+        private bool _canBeShown = true;
+        private bool _canGoBack;
         private bool _canGoForward;
+        private bool _needsUserInteraction = true;
+
+        public bool CanBeShown
+        {
+            get { return _canBeShown; }
+            set { SetProperty(value, ref _canBeShown); }
+        }
+
+        public bool CanGoBack
+        {
+            get { return _canGoBack; }
+            set { SetProperty(value, ref _canGoBack); }
+        }
+
         public bool CanGoForward
         {
             get { return _canGoForward; }
             set { SetProperty(value, ref _canGoForward); }
         }
 
-        private bool _canGoBack;
-        public bool CanGoBack
+        public bool NeedsUserInteraction
         {
-            get { return _canGoBack; }
-            set { SetProperty(value, ref _canGoBack); }
+            get { return _needsUserInteraction; }
+            set { SetProperty(value, ref _needsUserInteraction); }
         }
     }
 }
