7c07789dc39b81e619d7f41fbd94d1c8016adbef
Dominic Beger <admin@trade-programming.de>
2017-07-26 00:40:19 +0200

    Update installer binaries

--- nUpdate/Properties/Resources.Designer.cs
+++ nUpdate/Properties/Resources.Designer.cs
@@ -109,5 +109,15 @@ namespace nUpdate.Properties {
                 return ((byte[])(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] nUpdate_UpdateInstaller1 {
+            get {
+                object obj = ResourceManager.GetObject("nUpdate_UpdateInstaller1", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
     }
 }
