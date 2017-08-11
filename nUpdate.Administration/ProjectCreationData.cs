b72d35b8706a0887d03d1f4aa19e63803cb1cf0e
Dominic Beger <info@dominic-beger.de>
2017-08-11 18:20:21 +0200

    Add further properties to ProjectCreationData

--- nUpdate.Administration/ProjectCreationData.cs
+++ nUpdate.Administration/ProjectCreationData.cs
@@ -1,8 +1,22 @@
-﻿namespace nUpdate.Administration
+﻿// Author: Dominic Beger (Trade/ProgTrade) 2017
+
+namespace nUpdate.Administration
 {
     public class ProjectCreationData
     {
+        /// <summary>
+        ///     Gets or sets the location (directory path) of the project.
+        /// </summary>
+        public string Location { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the private key associated with the project.
+        /// </summary>
         public string PrivateKey { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the project data.
+        /// </summary>
         public UpdateProject Project { get; set; } = new UpdateProject();
     }
 }
