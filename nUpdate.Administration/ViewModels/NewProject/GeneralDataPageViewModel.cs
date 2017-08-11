ab866a30b950d18465a332fa1e68b9a53c2f6c83
Dominic Beger <info@dominic-beger.de>
2017-08-11 18:25:01 +0200

    Add further checks according to the navigation in GeneralDataPageViewModel

--- nUpdate.Administration/ViewModels/NewProject/GeneralDataPageViewModel.cs
+++ nUpdate.Administration/ViewModels/NewProject/GeneralDataPageViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Author: Dominic Beger (Trade/ProgTrade) 2017
+
+using System;
 using System.IO;
 using System.Windows.Input;
 using nUpdate.Administration.Infrastructure;
@@ -8,18 +10,34 @@ namespace nUpdate.Administration.ViewModels.NewProject
     public class GeneralDataPageViewModel : PageViewModel
     {
         private readonly NewProjectViewModel _newProjectViewModel;
+        private ICommand _loadCommand;
+        private string _location;
         private string _name;
         private string _updateDirectory;
-        private string _location;
-        private ICommand _loadCommand;
 
         public GeneralDataPageViewModel(NewProjectViewModel viewModel)
         {
             _newProjectViewModel = viewModel;
             CanGoBack = true;
 
-            Location = FilePathProvider.DefaultProjectsDirectory;
+            Location = PathProvider.DefaultProjectDirectory;
             LoadCommand = new RelayCommand(OnLoad);
+        }
+
+        public ICommand LoadCommand
+        {
+            get { return _loadCommand; }
+            set { SetProperty(value, ref _loadCommand, nameof(LoadCommand)); }
+        }
+
+        public string Location
+        {
+            get { return _location; }
+            set
+            {
+                SetProperty(value, ref _location, nameof(Location));
+                RefreshNavigation();
+            }
         }
 
         public string Name
@@ -28,7 +46,7 @@ namespace nUpdate.Administration.ViewModels.NewProject
             set
             {
                 SetProperty(value, ref _name, nameof(Name));
-                OnDataChange();
+                RefreshNavigation();
             }
         }
 
@@ -38,49 +56,45 @@ namespace nUpdate.Administration.ViewModels.NewProject
             set
             {
                 SetProperty(value, ref _updateDirectory, nameof(UpdateDirectory));
-                OnDataChange();
+                RefreshNavigation();
             }
-        }
-
-        public string Location
-        {
-            get { return _location; }
-            set
-            {
-                SetProperty(value, ref _location, nameof(Location));
-                OnDataChange();
-            }
-        }
-
-        public ICommand LoadCommand
-        {
-            get { return _loadCommand; }
-            set { SetProperty(value, ref _loadCommand, nameof(LoadCommand)); }
-        }
-        
-        private void OnDataChange()
-        {
-            Uri updateDirectoryUri;
-            CanGoForward = !string.IsNullOrEmpty(Name) && !string.IsNullOrEmpty(Location) && Uri.TryCreate(UpdateDirectory, UriKind.Absolute, out updateDirectoryUri);
         }
 
         private void OnLoad()
         {
-            if (!Directory.Exists(FilePathProvider.DefaultProjectsDirectory))
-                Directory.CreateDirectory(FilePathProvider.DefaultProjectsDirectory);
-
-            // Initialize the default text values
+            if (!Directory.Exists(PathProvider.DefaultProjectDirectory))
+                Directory.CreateDirectory(PathProvider.DefaultProjectDirectory);
+            
             var nameAvailable = false;
             var targetDirectory = default(DirectoryInfo);
             while (!nameAvailable)
             {
                 var i = 0;
-                targetDirectory = new DirectoryInfo(Path.Combine(FilePathProvider.DefaultProjectsDirectory, $"NewProject{(i > 0 ? i.ToString() : string.Empty)}"));
+                targetDirectory = new DirectoryInfo(Path.Combine(PathProvider.DefaultProjectDirectory,
+                    $"NewProject{(i > 0 ? i.ToString() : string.Empty)}"));
                 if (!(nameAvailable = !targetDirectory.Exists))
                     ++i;
             }
 
             Name = targetDirectory.Name;
+        }
+
+        private void RefreshNavigation()
+        {
+            Uri updateDirectoryUri;
+            CanGoForward = !string.IsNullOrEmpty(Name) && !string.IsNullOrEmpty(Location) &&
+                           Uri.TryCreate(UpdateDirectory, UriKind.Absolute, out updateDirectoryUri);
+
+            // If the data is okay, we will set it directly.
+            if (CanGoForward)
+                RefreshProjectData();
+        }
+
+        private void RefreshProjectData()
+        {
+            _newProjectViewModel.ProjectCreationData.Project.Name = Name;
+            _newProjectViewModel.ProjectCreationData.Project.UpdateDirectory = new Uri(UpdateDirectory);
+            _newProjectViewModel.ProjectCreationData.Location = Location;
         }
     }
 }
