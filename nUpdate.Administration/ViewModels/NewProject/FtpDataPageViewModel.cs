17214e037096b50164707a45d992176ce18fbd17
Dominic Beger <info@dominic-beger.de>
2017-08-24 16:09:25 +0200

    Finish first implementation of FtpDataPage

--- nUpdate.Administration/ViewModels/NewProject/FtpDataPageViewModel.cs
+++ nUpdate.Administration/ViewModels/NewProject/FtpDataPageViewModel.cs
@@ -1,12 +1,93 @@
-﻿namespace nUpdate.Administration.ViewModels.NewProject
+﻿// Author: Dominic Beger (Trade/ProgTrade) 2017
+
+using nUpdate.Administration.Ftp;
+using Starksoft.Aspen.Ftps;
+
+namespace nUpdate.Administration.ViewModels.NewProject
 {
     public class FtpDataPageViewModel : PageViewModel
     {
         private readonly NewProjectViewModel _newProjectViewModel;
+        private readonly FtpData _transferData;
+        private string _directory;
+        private string _host;
+        private string _password;
+        private int _port;
+        private FtpsSecurityProtocol _protocol;
+        private int _selectedModeIndex;
+        private string _username;
 
         public FtpDataPageViewModel(NewProjectViewModel viewModel)
         {
             _newProjectViewModel = viewModel;
+            _directory = "/";
+            _transferData = new FtpData();
+
+            PropertyChanged += (sender, args) => RefreshNavigation();
+        }
+
+        public string Directory
+        {
+            get => _directory;
+            set => SetProperty(value, ref _directory, nameof(Directory));
+        }
+
+        public string Host
+        {
+            get => _host;
+            set => SetProperty(value, ref _host, nameof(Host));
+        }
+
+        public string Password
+        {
+            get => _password;
+            set => SetProperty(value, ref _password, nameof(Password));
+        }
+
+        public int Port
+        {
+            get => _port;
+            set => SetProperty(value, ref _port, nameof(Port));
+        }
+
+        public FtpsSecurityProtocol Protocol
+        {
+            get => _protocol;
+            set => SetProperty(value, ref _protocol, nameof(Protocol));
+        }
+
+        public int SelectedModeIndex
+        {
+            get => _selectedModeIndex;
+            set => SetProperty(value, ref _selectedModeIndex, nameof(SelectedModeIndex));
+        }
+
+        public string Username
+        {
+            get => _username;
+            set => SetProperty(value, ref _username, nameof(Username));
+        }
+
+        private void RefreshNavigation()
+        {
+            CanGoForward = !string.IsNullOrEmpty(Host) && !string.IsNullOrEmpty(Username) &&
+                           !string.IsNullOrEmpty(Directory);
+
+            // If the data is okay, we will set it directly.
+            if (CanGoForward)
+                RefreshProjectData();
+        }
+
+        private void RefreshProjectData()
+        {
+            _transferData.Host = Host;
+            _transferData.Port = Port;
+            _transferData.Username = Username;
+            _transferData.UsePassiveMode = SelectedModeIndex == 0;
+            _transferData.FtpSpecificProtocol = Protocol;
+            _transferData.Directory = Directory;
+
+            _newProjectViewModel.ProjectCreationData.Project.TransferData = _transferData;
         }
     }
 }
--- nUpdate.Administration/Views/NewProject/FtpDataPage.xaml.cs
+++ nUpdate.Administration/Views/NewProject/FtpDataPage.xaml.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿// Author: Dominic Beger (Trade/ProgTrade) 2017
+
 using System.Windows;
-using Starksoft.Aspen.Ftps;
+using System.Windows.Controls;
 
 namespace nUpdate.Administration.Views.NewProject
 {
     /// <summary>
-    /// Interaktionslogik für FtpDataPage.xaml
+    ///     Interaktionslogik für FtpDataPage.xaml
     /// </summary>
     public partial class FtpDataPage
     {
@@ -17,7 +18,18 @@ namespace nUpdate.Administration.Views.NewProject
         private void FtpDataPage_OnLoaded(object sender, RoutedEventArgs e)
         {
             ModeComboBox.ItemsSource = new[] {"Passive (recommended)", "Active"};
-            ProtocolComboBox.ItemsSource = Enum.GetValues(typeof(FtpsSecurityProtocol));
+            ProtocolComboBox.ItemsSource = new[]
+            {
+                "FTP (insecure)", "FTPS (TLS1 Explicit)", "FTPS (TLS1 or SSL3 Explicit)", "FTPS (SSL3 explicit)",
+                "FTPS (SSL2 explicit)", "FTPS (TLS1 implicit)", "FTPS (TLS1 or SSL3 implicit)", "FTPS (SSL3 implicit)",
+                "FTPS (SSL2 implicit)"
+            };
+        }
+
+        private void PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (DataContext != null)
+                ((dynamic)DataContext).Password = ((PasswordBox)sender).Password;
         }
     }
 }
