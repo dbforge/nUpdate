221fa997d124a235fe6a4ea33945edef61b3437a
Dominic Beger <info@dominic-beger.de>
2017-08-27 18:41:44 +0200

    Make the FtpBrowseWindow a simple dialog without using MVVM

--- nUpdate.Administration/Views/FtpBrowseWindow.xaml.cs
+++ nUpdate.Administration/Views/FtpBrowseWindow.xaml.cs
@@ -1,16 +1,92 @@
-﻿namespace nUpdate.Administration.Views
+﻿// Author: Dominic Beger (Trade/ProgTrade) 2017
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using TaskDialogInterop;
+
+namespace nUpdate.Administration.Views
 {
-    /// <summary>
-    /// Interaktionslogik für FtpBrowseWindow.xaml
-    /// </summary>
     public partial class FtpBrowseWindow
     {
+        private readonly List<TreeViewItem> _cachedTreeViewItems = new List<TreeViewItem>();
+        private readonly TransferManager _transferManager;
+        private string _directory = "/";
+
         public FtpBrowseWindow(ITransferData data)
         {
             InitializeComponent();
-            ((dynamic) DataContext).TransferManager = new TransferManager(TransferProtocol.FTP, data);
+            _transferManager = new TransferManager(TransferProtocol.FTP, data);
         }
 
-        public string Directory => ((dynamic) DataContext).Directory;
+        public string Directory
+        {
+            get => _directory;
+            set
+            {
+                _directory = value;
+                DirectoryTextBox.Text = value;
+            }
+        }
+
+        private async Task AddDirectoryContent(string path, ItemsControl current)
+        {
+            var serverItems = await _transferManager.List(path, false);
+            foreach (var serverItem in serverItems.Where(x => x.ItemType == ServerItemType.Directory))
+                current.Items.Add(new TreeViewItem {Header = serverItem.Name});
+        }
+
+        private async void FtpBrowseWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            await AddDirectoryContent("/", (TreeViewItem)ServerTreeView.Items[0]);
+        }
+
+        private async void TreeView_OnSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            var selectedItem = (TreeViewItem) e.NewValue;
+
+            // Update the path
+            UpdatePath(selectedItem);
+
+            // If the content of the selected item has already been listed, we can ignore it
+            if (_cachedTreeViewItems.Contains(selectedItem))
+                return;
+
+            ServerTreeView.IsEnabled = false;
+            _cachedTreeViewItems.Add(selectedItem);
+
+            try
+            {
+                await AddDirectoryContent(Directory, selectedItem);
+            }
+            catch (Exception ex)
+            {
+                var taskDialog = new TaskDialogOptions
+                {
+                    MainIcon = VistaTaskDialogIcon.Error,
+                    MainInstruction = ex.Message,
+                    ExpandedInfo = ex.StackTrace
+                };
+                TaskDialog.Show(taskDialog);
+            }
+
+            ServerTreeView.IsEnabled = true;
+        }
+
+        private void UpdatePath(TreeViewItem current)
+        {
+            var itemStack = new Stack<TreeViewItem>();
+            var currentItem = current;
+            while (currentItem != null)
+            {
+                itemStack.Push(currentItem);
+                currentItem = (TreeViewItem) currentItem.Parent;
+            }
+
+            Directory = string.Join("/", itemStack.Select(x => x.Header.ToString()));
+        }
     }
 }
