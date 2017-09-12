0049d8e12b8f323916926d72da366439631d97cb
Dominic Beger <info@dominic-beger.de>
2017-09-12 23:25:21 +0200

    Update version to v3.2

--- nUpdate.Client.GuiInterface/Properties/AssemblyInfo.cs
+++ nUpdate.Client.GuiInterface/Properties/AssemblyInfo.cs
@@ -37,5 +37,5 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.8.0")]
+[assembly: AssemblyVersion("3.2.0.0")]
+[assembly: AssemblyFileVersion("3.2.0.0")]
