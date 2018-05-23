3d3f5bf90bdbc4f1fdfbc043f06d32c2bb59ecf0
Dominic Beger <info@dominic-beger.de>
2018-05-23 20:22:18 +0200

    Update version to v3.3

--- nUpdate.Administration/Program.cs
+++ nUpdate.Administration/Program.cs
@@ -57,7 +57,7 @@ namespace nUpdate.Administration
         /// <summary>
         ///     The version string shown in all dialog titles.
         /// </summary>
-        public static string VersionString => "nUpdate Administration v3.2.1";
+        public static string VersionString => "nUpdate Administration v3.3.0";
 
         private static void Exit(object sender, EventArgs e)
         {
