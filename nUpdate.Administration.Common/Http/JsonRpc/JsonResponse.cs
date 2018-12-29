6162f4b5053ccbd865b7fb69a13427c935fba9b0
Dominic Beger <info@dominic-beger.de>
2018-12-29 23:37:18 +0100

    Add preparations for JsonRPC

---
+++ nUpdate.Administration.Common/Http/JsonRpc/JsonResponse.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+
+namespace nUpdate.Administration.Common.Http.JsonRpc
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class JsonResponse
+    {
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "jsonrpc")]
+        public string JsonRpc => "2.0";
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "result")]
+        public object Result { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "error")]
+        public JsonRpcException Error { get; set; }
+
+        [JsonProperty(PropertyName = "id")]
+        public object Id { get; set; }
+    }
+
+    /// <summary>
+    /// Represents a Json Rpc Response
+    /// </summary>
+    [JsonObject(MemberSerialization.OptIn)]
+    public class JsonResponse<T>
+    {
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "jsonrpc")]
+        public string JsonRpc => "2.0";
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "result")]
+        public T Result { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "error")]
+        public JsonRpcException Error { get; set; }
+
+        [JsonProperty(PropertyName = "id")]
+        public object Id { get; set; }
+    }
+}
